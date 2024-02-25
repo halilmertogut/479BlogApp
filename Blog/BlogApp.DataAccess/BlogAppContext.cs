4e919a952c2b309b60c4d7c815552efaa6d27373
Halil Mert Öğüt <101225219+halilmertogut@users.noreply.github.com>
2024-02-25 23:57:37 +0300

    feat(BlogApp.cs): assignment done

---
+++ Blog/BlogApp.DataAccess/BlogAppContext.cs
@@ -0,0 +1,48 @@
+﻿using BlogApp.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+
+namespace BlogApp.DataAccess;
+
+public class BlogAppContext : DbContext
+{
+    public BlogAppContext(DbContextOptions<BlogAppContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<PostTag> PostTags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<PostTag>()
+            .HasKey(pt => new { pt.PostId, pt.TagId });
+
+
+        modelBuilder.Entity<Post>()
+            .HasMany(p => p.Comments)
+            .WithOne(c => c.Post)
+            .HasForeignKey(c => c.PostId);
+
+        modelBuilder.Entity<Category>()
+            .HasMany(c => c.Posts)
+            .WithOne(); 
+
+        modelBuilder.Entity<PostTag>()
+            .HasOne(pt => pt.Post)
+            .WithMany(p => p.PostTags)
+            .HasForeignKey(pt => pt.PostId);
+
+        modelBuilder.Entity<PostTag>()
+            .HasOne(pt => pt.Tag)
+            .WithMany(t => t.PostTags)
+            .HasForeignKey(pt => pt.TagId);
+    }
+}
---
+++ Blog/BlogApp.DataAccess/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BlogApp.DataAccess
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Comment> Comments { get; set; } 
+        public ICollection<PostTag> PostTags { get; set; } 
+    }
+
+
+}
